975bdf3013ad6d4f51ada0b7367dc8f57a2d1eb6
Mark Wubben <mark@novemberborn.net>
2017-02-27 14:45:52 +0000

    Implementation

---
+++ test.js
@@ -0,0 +1,32 @@
+import test from 'ava'
+
+import { isWellKnown, getLabel } from './'
+
+const names = [
+  'iterator',
+  'match',
+  'replace',
+  'search',
+  'split',
+  'hasInstance',
+  'isConcatSpreadable',
+  'unscopables',
+  'species',
+  'toPrimitive',
+  'toStringTag'
+]
+
+const wellKnown = (t, name, expected) => {
+  t.is(isWellKnown(Symbol[name]), expected)
+}
+wellKnown.title = (_, name, expected) => `Symbol.${name} is ${expected ? 'well known' : 'not well known'} (on this platform)`
+
+const label = (t, name, expected) => {
+  t.is(getLabel(Symbol[name]), expected)
+}
+label.title = (_, name, expected) => `Label of Symbol.${name} is ${expected === undefined ? 'undefined' : `'${expected}'`} (on this platform)`
+
+for (const name of names) {
+  test(wellKnown, name, Symbol[name] !== undefined)
+  test(label, name, Symbol[name] === undefined ? undefined : `Symbol.${name}`)
+}
